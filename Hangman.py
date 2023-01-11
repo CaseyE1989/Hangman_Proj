10591b276184b542a9b885ac62b80e13c9886de8
CaseyE1989 <118781940+CaseyE1989@users.noreply.github.com>
2023-01-10 20:24:10 -0700

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,14 +1,293 @@
 import random
-import hangman_words
-import hangman_art
+word_list = [
+'abruptly',
+'absurd',
+'abyss',
+'affix',
+'askew',
+'avenue',
+'awkward',
+'axiom',
+'azure',
+'bagpipes',
+'bandwagon',
+'banjo',
+'bayou',
+'beekeeper',
+'bikini',
+'blitz',
+'blizzard',
+'boggle',
+'bookworm',
+'boxcar',
+'boxful',
+'buckaroo',
+'buffalo',
+'buffoon',
+'buxom',
+'buzzard',
+'buzzing',
+'buzzwords',
+'caliph',
+'cobweb',
+'cockiness',
+'croquet',
+'crypt',
+'curacao',
+'cycle',
+'daiquiri',
+'dirndl',
+'disavow',
+'dizzying',
+'duplex',
+'dwarves',
+'embezzle',
+'equip',
+'espionage',
+'euouae',
+'exodus',
+'faking',
+'fishhook',
+'fixable',
+'fjord',
+'flapjack',
+'flopping',
+'fluffiness',
+'flyby',
+'foxglove',
+'frazzled',
+'frizzled',
+'fuchsia',
+'funny',
+'gabby',
+'galaxy',
+'galvanize',
+'gazebo',
+'giaour',
+'gizmo',
+'glowworm',
+'glyph',
+'gnarly',
+'gnostic',
+'gossip',
+'grogginess',
+'haiku',
+'haphazard',
+'hyphen',
+'iatrogenic',
+'icebox',
+'injury',
+'ivory',
+'ivy',
+'jackpot',
+'jaundice',
+'jawbreaker',
+'jaywalk',
+'jazziest',
+'jazzy',
+'jelly',
+'jigsaw',
+'jinx',
+'jiujitsu',
+'jockey',
+'jogging',
+'joking',
+'jovial',
+'joyful',
+'juicy',
+'jukebox',
+'jumbo',
+'kayak',
+'kazoo',
+'keyhole',
+'khaki',
+'kilobyte',
+'kiosk',
+'kitsch',
+'kiwifruit',
+'klutz',
+'knapsack',
+'larynx',
+'lengths',
+'lucky',
+'luxury',
+'lymph',
+'marquis',
+'matrix',
+'megahertz',
+'microwave',
+'mnemonic',
+'mystify',
+'naphtha',
+'nightclub',
+'nowadays',
+'numbskull',
+'nymph',
+'onyx',
+'ovary',
+'oxidize',
+'oxygen',
+'pajama',
+'peekaboo',
+'phlegm',
+'pixel',
+'pizazz',
+'pneumonia',
+'polka',
+'pshaw',
+'psyche',
+'puppy',
+'puzzling',
+'quartz',
+'queue',
+'quips',
+'quixotic',
+'quiz',
+'quizzes',
+'quorum',
+'razzmatazz',
+'rhubarb',
+'rhythm',
+'rickshaw',
+'schnapps',
+'scratch',
+'shiv',
+'snazzy',
+'sphinx',
+'spritz',
+'squawk',
+'staff',
+'strength',
+'strengths',
+'stretch',
+'stronghold',
+'stymied',
+'subway',
+'swivel',
+'syndrome',
+'thriftless',
+'thumbscrew',
+'topaz',
+'transcript',
+'transgress',
+'transplant',
+'triphthong',
+'twelfth',
+'twelfths',
+'unknown',
+'unworthy',
+'unzip',
+'uptown',
+'vaporize',
+'vixen',
+'vodka',
+'voodoo',
+'vortex',
+'voyeurism',
+'walkway',
+'waltz',
+'wave',
+'wavy',
+'waxy',
+'wellspring',
+'wheezy',
+'whiskey',
+'whizzing',
+'whomever',
+'wimpy',
+'witchcraft',
+'wizard',
+'woozy',
+'wristwatch',
+'wyvern',
+'xylophone',
+'yachtsman',
+'yippee',
+'yoked',
+'youthful',
+'yummy',
+'zephyr',
+'zigzag',
+'zigzagging',
+'zilch',
+'zipper',
+'zodiac',
+'zombie',
+]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
-chosen_word = random.choice(hangman_words.word_list)
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+chosen_word = random.choice(word_list)
 display = []
 end_of_game = False
 lives = 6
 previous_guess = []
 
-print(hangman_art.logo)
+print(logo)
 
 for _ in range(len(chosen_word)): #Display blanks '_','_','_'
     display += '_'
@@ -33,7 +312,7 @@ while not end_of_game:
         lives -= 1
         print(f'You chose {guess}. The letter is not in the answer. You lose a life!')
         previous_guess.append(guess)
-        print(hangman_art.stages[lives])
+        print(stages[lives])
 
     if "_" not in display:
         end_of_game = True
@@ -42,4 +321,4 @@ while not end_of_game:
         end_of_game = True
         print('You lose!')
         print(f'The word was {chosen_word}.')
-        input()
+input()
